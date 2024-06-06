ea2a15bcab776f32f0e811adb6f7d07b8d0137c8
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-06-06 14:34:41 -0400

    initial struct and reflections

--- src/tract.rs
+++ src/tract.rs
@@ -5,8 +5,12 @@ const SPEED_OF_SOUND: f32 = 343.0; /* m/s @ 20C */
 pub struct Nose {
     left: Vec<f32>,
     right: Vec<f32>,
+    junc_left: Vec<f32>,
+    junc_right: Vec<f32>,
     areas: Vec<f32>,
+    diams: Vec<f32>,
     reflections: Vec<f32>,
+    length: usize,
 }
 
 pub struct Tract {
@@ -247,14 +251,62 @@ impl Tract {
 }
 
 impl Nose {
-    // pub fn new(sr: usize, length: f32, oversample: u16) -> Self {
-    // }
+    pub fn new(sr: usize, length: f32, oversample: u16) -> Self {
+        let nose_length =
+            (((length * 0.01) / (SPEED_OF_SOUND as f32 / (sr as f32 * oversample as f32)))).floor() + 1.0;
+
+        let nose_length = nose_length as usize;
+
+        let mut ns = Nose {
+            areas: vec![0.0; nose_length],
+            left: vec![0.0; nose_length],
+            right: vec![0.0; nose_length],
+            junc_left: vec![0.0; nose_length],
+            junc_right: vec![0.0; nose_length],
+            reflections: vec![0.0; nose_length],
+            diams: vec![0.0; nose_length],
+            length: nose_length,
+        };
+
+        ns.setup_shape();
+
+        ns
+    }
     fn setup_shape(&mut self) {
-        // TODO: implement
+        let diams = &mut self.diams;
+
+        for i in 0 .. self.length {
+            let mut d = 2.0 * (i as f32 / self.length as f32);
+            
+            if d < 1.0 {
+                d = 0.4 + 1.6*d;
+            } else {
+                d = 0.5 + 1.5*(2.0 - d);
+            }
+
+            if d > 1.9 {
+                d = 1.9;
+            }
+
+            diams[i] = d;
+        }
+        self.calculate_reflections();
+
     }
 
-    // TODO implement
     fn calculate_reflections(&mut self) {
+        let areas = &mut self.areas;
+        let diams = &self.diams;
+        let refl = &mut self.reflections;
+        for i in 0 .. self.length {
+            areas[i] = diams[i]*diams[i];
+        } 
+
+        for i in 1 .. self.length {
+            refl[i] =
+                (areas[i - 1] - areas[i]) /
+                (areas[i - 1] + areas[i]);
+        }
     }
 
     // TODO implement tick
