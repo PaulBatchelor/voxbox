cf6094f4c404acb124c70836cfaa45bfda2b8b34
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-03 10:00:32 -0400

    RandomLine: initial implementation

--- src/rng.rs
+++ src/rng.rs
@@ -1,4 +1,4 @@
-// TODO Make this a generic
+// TODO Make this a generic?
 pub struct LinearCongruentialGenerator {
     rng: u32,
 }
@@ -22,5 +22,61 @@ impl LinearCongruentialGenerator {
 
     pub fn seed(&mut self, val: u32) {
         self.rng = val;
+    }
+}
+
+pub struct RandomLine {
+    rng: LinearCongruentialGenerator,
+    pub min: f32,
+    pub max: f32,
+    lphs: f32,
+    val_a: f32,
+    val_b: f32,
+}
+
+impl RandomLine {
+    pub fn new(&mut self) -> Self {
+        RandomLine {
+            lphs: -1.0,
+            val_a: 0.0,
+            val_b: 0.0,
+            min: 0.0,
+            max: 1.0,
+            rng: LinearCongruentialGenerator::new(),
+        }
+    }
+
+    pub fn seed(&mut self, val: u32) {
+        self.rng.seed(val);
+    }
+
+    pub fn tick(&mut self, phs: f32) -> f32 {
+        let lphs = self.lphs;
+
+        if lphs < 0.  {
+            // (re-)initialize generator, generate
+            // two new random values
+
+            self.val_a = self.rng.randf();
+            self.val_b = self.rng.randf();
+        } else if phs < lphs {
+            // New Period. Update points.
+            
+            self.val_a = self.val_b;
+            self.val_b = self.rng.randf();
+        }
+
+
+        // scale and interpolate
+        let diff = self.max - self.min;
+        let min = self.min;
+        let a = self.val_a*diff + min;
+        let b = self.val_b*diff + min;
+
+        let out = (1.0 - phs)*a + phs*b;
+
+        self.lphs = phs;
+
+        out
     }
 }
