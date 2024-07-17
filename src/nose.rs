208aeb2e384359072d80541993c873bdc04f02dd
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-17 10:38:24 -0400

    quick clippy error

--- src/nose.rs
+++ src/nose.rs
@@ -18,13 +18,14 @@ pub struct Nose {
     length_max: usize,
     velum: f32,
     sr: usize,
-    oversample:u16,
+    oversample: u16,
 }
 
 impl Nose {
     pub fn new(sr: usize, length: f32, oversample: u16) -> Self {
         let nose_length =
-            (((length * 0.01) / (SPEED_OF_SOUND as f32 / (sr as f32 * oversample as f32)))).floor() + 1.0;
+            ((length * 0.01) / (SPEED_OF_SOUND as f32 / (sr as f32 * oversample as f32))).floor()
+                + 1.0;
 
         let nose_length = nose_length as usize;
 
@@ -53,20 +54,18 @@ impl Nose {
     fn setup_shape(&mut self) {
         let diams = &mut self.diams;
 
-        for i in 0 .. self.length {
+        for i in 0..self.length {
             let mut d = 2.0 * (i as f32 / self.length as f32);
 
             if d < 1.0 {
-                d = 0.4 + 1.6*d;
+                d = 0.4 + 1.6 * d;
             } else {
-                d = 0.5 + 1.5*(2.0 - d);
+                d = 0.5 + 1.5 * (2.0 - d);
             }
 
             // silly translation of ternary expression,
             // for debugging purposes
-            if d < 1.9 {
-                d = d;
-            } else {
+            if d > 1.9 {
                 d = 1.9;
             }
 
@@ -83,26 +82,19 @@ impl Nose {
         let areas = &mut self.areas;
         let diams = &self.diams;
         let refl = &mut self.reflections;
-        for i in 0 .. self.length {
-            areas[i] = diams[i]*diams[i];
+        for i in 0..self.length {
+            areas[i] = diams[i] * diams[i];
         }
 
-        for i in 1 .. self.length {
-            refl[i] =
-                (areas[i - 1] - areas[i]) /
-                (areas[i - 1] + areas[i]);
+        for i in 1..self.length {
+            refl[i] = (areas[i - 1] - areas[i]) / (areas[i - 1] + areas[i]);
         }
     }
 
-
-    pub fn calculate_reflections_with_tract(&mut self, tr: &Tract, nose_start: usize)
-    {
+    pub fn calculate_reflections_with_tract(&mut self, tr: &Tract, nose_start: usize) {
         self.diams[0] = self.velum;
-        self.areas[0] = self.diams[0]*self.diams[0];
-        let sum =
-            tr.areas[nose_start] +
-            tr.areas[nose_start + 1] +
-            self.areas[0];
+        self.areas[0] = self.diams[0] * self.diams[0];
+        let sum = tr.areas[nose_start] + tr.areas[nose_start + 1] + self.areas[0];
         self.reflection_left = (2.0 * tr.areas[nose_start] - sum) / sum;
         self.reflection_right = (2.0 * tr.areas[nose_start + 1] - sum) / sum;
         self.reflection_nose = (2.0 * self.areas[0] - sum) / sum;
@@ -139,9 +131,7 @@ impl Nose {
             // panic!("NAN");
         }
 
-        tr_jl[nose_start - 1] =
-            r*tr_r[nose_start - 1] +
-            (1.0 + r)*(ns_l[0] + tr_l[nose_start]);
+        tr_jl[nose_start - 1] = r * tr_r[nose_start - 1] + (1.0 + r) * (ns_l[0] + tr_l[nose_start]);
 
         if tr_jl[nose_start - 1].is_nan() {
             // dbg!(self.samppos);
@@ -152,9 +142,7 @@ impl Nose {
 
         // TODO check this equation, it looks wrong.
         // shouldn't it match junc_left more?
-        tr_jr[nose_start] =
-            r*tr_l[nose_start] +
-            (1.0 + r)*(tr_r[nose_start - 1] + ns_l[0]);
+        tr_jr[nose_start] = r * tr_l[nose_start] + (1.0 + r) * (tr_r[nose_start - 1] + ns_l[0]);
 
         if tr_jr[nose_start].is_nan() {
             // dbg!(self.samppos);
@@ -176,27 +164,22 @@ impl Nose {
         }
 
         let r = self.reflection_nose;
-        ns_jr[0] =
-            r*ns_l[0] +
-            (1.0+r)*(tr_l[nose_start]+tr_r[nose_start - 1]);
+        ns_jr[0] = r * ns_l[0] + (1.0 + r) * (tr_l[nose_start] + tr_r[nose_start - 1]);
 
         if ns_jr[0].is_nan() {
             // dbg!(self.samppos);
             // panic!("NAN");
         }
 
-        ns_jl[self.length - 1] =
-            tr.get_lip_reflection() *ns_r[self.length - 1];
+        ns_jl[self.length - 1] = tr.get_lip_reflection() * ns_r[self.length - 1];
 
         if ns_jl[self.length - 1].is_nan() {
             // dbg!(self.samppos);
             // panic!("NAN");
         }
 
-        for i in 1 .. self.length {
-            let w =
-                self.reflections[i] *
-                (ns_r[i - 1] + ns_l[i]);
+        for i in 1..self.length {
+            let w = self.reflections[i] * (ns_r[i - 1] + ns_l[i]);
             if w.is_nan() {
                 // dbg!(self.samppos);
                 // panic!("NAN");
@@ -205,7 +188,7 @@ impl Nose {
             ns_jl[i - 1] = ns_l[i] + w;
         }
 
-        for i in 0 .. self.length {
+        for i in 0..self.length {
             ns_l[i] = ns_jl[i];
             ns_r[i] = ns_jr[i];
         }
@@ -214,10 +197,10 @@ impl Nose {
     }
 
     pub fn set_length(&mut self, len_cm: f32) {
-        let tractlen =
-            (((len_cm * 0.01) /
-              (SPEED_OF_SOUND as f32 /
-               (self.sr as f32 * self.oversample as f32)))).floor() + 1.0;
+        let tractlen = ((len_cm * 0.01)
+            / (SPEED_OF_SOUND as f32 / (self.sr as f32 * self.oversample as f32)))
+            .floor()
+            + 1.0;
 
         let mut tractlen = tractlen as usize;
 
