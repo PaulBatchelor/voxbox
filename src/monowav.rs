d63bb6e60d10943418c5589d01585a9233eacdf9
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-07-17 10:26:08 -0400

    monowav: write -> write_all makes clippy happy

--- src/monowav.rs
+++ src/monowav.rs
@@ -27,44 +27,44 @@ impl MonoWav {
     }
 
     fn write_master_chunk(wav: &mut BufWriter<File>) {
-        wav.write(b"RIFF").unwrap();
-        wav.write(&0u32.to_le_bytes()).unwrap();
-        wav.write(b"WAVE").unwrap();
+        wav.write_all(b"RIFF").unwrap();
+        wav.write_all(&0u32.to_le_bytes()).unwrap();
+        wav.write_all(b"WAVE").unwrap();
     }
 
     fn write_fmt_chunk(wav: &mut BufWriter<File>) {
-        wav.write(b"fmt ").unwrap();
+        wav.write_all(b"fmt ").unwrap();
 
         /* chunk size 16 (0x10) */
-        wav.write(&0x10u32.to_le_bytes()).unwrap();
+        wav.write_all(&0x10u32.to_le_bytes()).unwrap();
 
         /* format code: WAVE_FORMAT_PCM (0x0001) */
-        wav.write(&0x0001u16.to_le_bytes()).unwrap();
+        wav.write_all(&0x0001u16.to_le_bytes()).unwrap();
 
         /* nchannels: 1 (always mono) */
-        wav.write(&0x0001u16.to_le_bytes()).unwrap();
+        wav.write_all(&0x0001u16.to_le_bytes()).unwrap();
 
         /* sample rate 44.1kHz */
-        wav.write(&44100u32.to_le_bytes()).unwrap();
+        wav.write_all(&44100u32.to_le_bytes()).unwrap();
 
         /* bytes per second */
-        wav.write(&(44100u32 * 2).to_le_bytes()).unwrap();
+        wav.write_all(&(44100u32 * 2).to_le_bytes()).unwrap();
 
         /* block alignment */
-        wav.write(&2u16.to_le_bytes()).unwrap();
+        wav.write_all(&2u16.to_le_bytes()).unwrap();
 
         /* block alignment */
-        wav.write(&16u16.to_le_bytes()).unwrap();
+        wav.write_all(&16u16.to_le_bytes()).unwrap();
     }
 
     fn write_data_chunk(wav: &mut BufWriter<File>) {
-        wav.write(b"data").unwrap();
-        wav.write(&0u32.to_le_bytes()).unwrap();
+        wav.write_all(b"data").unwrap();
+        wav.write_all(&0u32.to_le_bytes()).unwrap();
     }
 
     pub fn tick(&mut self, sig: f32) {
         let isig = (sig * 32767.0) as i16;
-        self.wav.write(&isig.to_le_bytes()).unwrap();
+        self.wav.write_all(&isig.to_le_bytes()).unwrap();
         self.nbytes += 2;
     }
 }
@@ -72,10 +72,12 @@ impl MonoWav {
 impl Drop for MonoWav {
     fn drop(&mut self) {
         self.wav.seek(SeekFrom::Start(0x04)).unwrap();
-        self.wav.write(&(self.nbytes + 16 + 16 + 4).to_le_bytes()).unwrap();
+        self.wav
+            .write_all(&(self.nbytes + 16 + 16 + 4).to_le_bytes())
+            .unwrap();
 
         self.wav.seek(SeekFrom::Start(0x28)).unwrap();
-        self.wav.write(&self.nbytes.to_le_bytes()).unwrap();
+        self.wav.write_all(&self.nbytes.to_le_bytes()).unwrap();
         self.wav.flush().unwrap();
     }
 }
